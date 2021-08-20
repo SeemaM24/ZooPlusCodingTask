2ddd7e1f72b12a5631c403092a5a2ae3870a928b
SeemaM24 <89252314+SeemaM24@users.noreply.github.com>
2021-08-20 22:15:43 +0200

    Coding Result

---
+++ PetStore/src/PetMaster.java
@@ -0,0 +1,97 @@
+import java.util.*;
+
+public class PetMaster {
+	
+	public static int PetIds[] = new int[3];
+	public static String PetNames[] = new String[3];
+	public static String PetStatus[] = new String[3];
+	
+	public void PetData()
+	{
+	
+		PetIds[0]= 1;
+		PetIds[1]= 2;
+		PetIds[2]= 3;
+		
+		PetNames[0]= "Tom";
+		PetNames[1]= "Jerry";
+		PetNames[2]= "Spike";
+		
+		PetStatus[0]= "Available";
+		PetStatus[1]= "Pending";
+		PetStatus[2]= "Sold";	
+		
+	System.out.println("Test");
+	}
+
+	// This Method Find/Get Pet's name by Pet Id
+	public static String GetPetbyID(int id)
+	{
+		int y = 0;	
+		for(int i = 0; i< 3; i++){
+
+			if (PetIds[i]== id)
+				y = i;
+			}
+		return PetNames[y];
+	}
+	
+	// This Method Find/Get Pet's name by Pet Status
+	public static String GetPetbyStatus(String st)
+	{
+		int x =0;
+		System.out.println(st);
+		for(int i = 0; i< 3; i++){
+			System.out.println("Status in for " +PetStatus[i]);
+			if (PetStatus[i]== st)
+			{
+				x = i;
+				System.out.println("In If " +x);
+			}
+			}
+		return PetNames[x];
+	}
+	
+	// This Method Deletes Pet for a Pet ID
+	public static String DeletePet(int delid)
+	{
+		String DelResult = "Pet Deleted";
+		
+		for(int i = 0; i< 3; i++){
+
+			if (PetIds[i]== delid)
+				{
+				PetIds[i] = 0;
+				PetNames[i]= " ";
+				PetStatus[i] = " ";
+				//return DelResult;
+				}
+			}
+		return DelResult;
+	}
+	public static void main(String[] args)
+	{
+		System.out.println("In Main");
+		PetMaster PM = new PetMaster();
+		PM.PetData();	
+		
+		String PetNameFound;
+		String DelMsg;
+		//The below code calls GetPetbyID Method
+		PetNameFound = GetPetbyID(1);
+		System.out.println(PetNameFound);
+		
+		//The below code calls GetPetbyStatus Method
+		PetNameFound = GetPetbyStatus("Pending");
+		System.out.println(PetNameFound);
+		
+		//The below code calls DeletePet Method
+		DelMsg = DeletePet(1);
+		System.out.println(DelMsg);
+		System.out.println(PetIds[0]);
+		System.out.println(PetNames[0]);
+		
+	}
+
+
+}
---
+++ PetStore/srctest/TestGetPetbyStatus.java
@@ -0,0 +1,35 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.*;
+
+public class TestGetPetbyStatus {
+
+
+	@Test
+	public void test1() {
+		
+		PetMaster PM = new PetMaster();
+		PM.PetData();
+		
+		String ActualPetName = PM.GetPetbyStatus("Sold");
+		System.out.println(ActualPetName);
+		String ExpectedPetName = "Spike";
+		assertEquals(ExpectedPetName,ActualPetName);
+		
+	}
+	
+	@Test
+	public void test2() {
+		
+		PetMaster PM = new PetMaster();
+		PM.PetData();
+		
+		String ActualPetName = PM.GetPetbyStatus("Available");
+		System.out.println(ActualPetName);
+		String ExpectedPetName = "Tom";
+		assertEquals(ExpectedPetName,ActualPetName);
+		
+	}
+
+}
